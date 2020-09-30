cf438489ef56fa35e3be1ed48aefb24ccc865be9
SJ <slgsunjian@gmail.com>
2020-09-30 12:39:37 +0800

    improve uart_printf code style

--- Core/Src/uart_printf.c
+++ Core/Src/uart_printf.c
@@ -1,10 +1,10 @@
-#include "stm32f1xx_hal.h"
-#include  <errno.h>
-#include  <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
+#include "stm32f1xx_hal.h"  // HAL_StatusTypeDef
+#include <errno.h>  // errno, EBADF
+#include <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
 
 extern UART_HandleTypeDef huart1;
 
-int _write(int file , char *data, int len)
+int _write(int file, char *data, int len)
 {
    if ((file != STDOUT_FILENO) && (file != STDERR_FILENO))
    {
@@ -13,8 +13,7 @@ int _write(int file , char *data, int len)
    }
 
    // arbitrary timeout 1000
-   HAL_StatusTypeDef status =
-      HAL_UART_Transmit(&huart1, (uint8_t*)data, len, 1000);
+   HAL_StatusTypeDef status = HAL_UART_Transmit(&huart1, (uint8_t*)data, len, 1000);
 
    // return # of bytes written - as best we can tell
    return (status == HAL_OK ? len : 0);
