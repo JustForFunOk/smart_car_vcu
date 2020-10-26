877c6b10e33f1dc65ef5869d65c3b73329f12074
SJ <slgsunjian@gmail.com>
2020-10-26 14:48:33 +0800

    add rtos and w5500 task in .ico

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -47,6 +48,20 @@ SPI_HandleTypeDef hspi2;
 
 UART_HandleTypeDef huart1;
 
+/* Definitions for TcpReceiveTask */
+osThreadId_t TcpReceiveTaskHandle;
+const osThreadAttr_t TcpReceiveTask_attributes = {
+  .name = "TcpReceiveTask",
+  .priority = (osPriority_t) osPriorityRealtime,
+  .stack_size = 128 * 4
+};
+/* Definitions for TcpTransmitTask */
+osThreadId_t TcpTransmitTaskHandle;
+const osThreadAttr_t TcpTransmitTask_attributes = {
+  .name = "TcpTransmitTask",
+  .priority = (osPriority_t) osPriorityHigh,
+  .stack_size = 128 * 4
+};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -56,6 +71,9 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_SPI2_Init(void);
+void startTcpReceiveTask(void *argument);
+void startTcpTransmitTask(void *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -195,6 +213,40 @@ int main(void)
   uint8_t recv_buff[2048];
   /* USER CODE END 2 */
 
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of TcpReceiveTask */
+  TcpReceiveTaskHandle = osThreadNew(startTcpReceiveTask, NULL, &TcpReceiveTask_attributes);
+
+  /* creation of TcpTransmitTask */
+  TcpTransmitTaskHandle = osThreadNew(startTcpTransmitTask, NULL, &TcpTransmitTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -375,6 +427,63 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_startTcpReceiveTask */
+/**
+  * @brief  Function implementing the TcpReceiveTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_startTcpReceiveTask */
+void startTcpReceiveTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_startTcpTransmitTask */
+/**
+* @brief Function implementing the TcpTransmitTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_startTcpTransmitTask */
+void startTcpTransmitTask(void *argument)
+{
+  /* USER CODE BEGIN startTcpTransmitTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END startTcpTransmitTask */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
