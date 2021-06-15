25a446a40515dc52e6d479cd541509dbab77c8cc
SJ <slgsunjian@gmail.com>
2021-06-15 22:38:52 +0800

    change mpu9250 AD0 to 1

--- Drivers/VCU/Inc/mpu9250_config.h
+++ Drivers/VCU/Inc/mpu9250_config.h
@@ -1,18 +1,25 @@
 #ifndef _MPU9250_CONFIG_H_
 #define _MPU9250_CONFIG_H_
 #include "main.h"
-// use IIC or not
-#define MPU9250_USE_IIC
+
+// Please check below configuration
+#define MPU9250_USE_IIC // use IIC or not
+#define MPU9250_AD0_PIN_IS_HIGH // the AD0 pin connect to VCC or not
+// Please check above configuration
+
+#ifdef MPU9250_AD0_PIN_IS_HIGH
+#define DEVICE_ADDRESS        0xD2  // AD0 = 1 11010010 left shift one bit in STM32 HAL
+#else
+#define DEVICE_ADDRESS        0xD0  // AD0 = 0 11010000 left shift one bit in STM32 HAL
+#endif
 
 #ifdef MPU9250_USE_IIC
 #define MPU9250_I2C_CHANNEL   hi2c1
-// AD = 0
-#define DEVICE_ADDRESS        0xD0
 #else
 #define MPU9250_SPI           hspi1
 #define MPU9250_CS_GPIO       MPU9250_CS_GPIO_Port
 #define MPU9250_CS_PIN        MPU9250_CS_Pin
-#endif  // #ifdef MPU9250_USE_IIC
+#endif
 
 // sensor parameter
 // A range
